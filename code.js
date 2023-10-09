f86bdaeb5d7266b65058761afd7461b12c5cf501
E <145878037+e-clipse-dev@users.noreply.github.com>
2023-10-08 21:55:40 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
+//1
 const past = (h, m, s) => ((3600000*h) + (60000*m) + (1000*s))
+
+//2
+const opposite = (num) => -num
+
+//3
+const evenOrOdd = num => (num % 2 === 0) ? 'Even' : 'Odd'
+
+//4
+const doubleInteger = i => i*2
+
+//5
+const removeExclamationMarks = s => s.replaceAll('!', '')
